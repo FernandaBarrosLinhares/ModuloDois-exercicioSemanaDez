6e1a67067aa7fbb5d2bdd4252617497cd36e8262
“FernandaBarrosLinhares” <fernanda_linhares@estudante.sesisenai.org.br>
2023-07-29 09:58:09 -0300

    first commit

---
+++ api/src/test/java/br/senai/lab365/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.lab365.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
